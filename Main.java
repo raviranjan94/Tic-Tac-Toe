68a47d007e66ceda398ca0a6a1518a9602c3c52a
Ravi Ranjan <111891524+raviranjan94@users.noreply.github.com>
2023-08-21 09:18:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.mygame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program started.....");
+        MyGame game = new MyGame();
+    }
+}
